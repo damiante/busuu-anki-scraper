27c3c3c7a2428e98bd6b197b80abb7814e4688b4
damiante <damian@damiantesta.com>
2025-06-03 21:36:12 +1000

    Reworked to handle vocab where examples are optional

--- parse-html.py
+++ parse-html.py
@@ -6,40 +6,84 @@ from html.parser import HTMLParser
 INPUT_FILE = "./vocab-page.html"
 OUTPUT_FILE = "./cards.txt" # Anki likes plaintext even if the format is csv
 
-JAPANESE_TEXT = []
-ENGLISH_TEXT = []
+CURRENT_CARD = None
+CARDS = []
+
+
+class Card():
+    def __init__(self):
+        self.jp_vocab = ""
+        self.en_vocab = ""
+        self.jp_example = ""
+        self.en_example = ""
+
+    def __repr__(self):
+        return f"<Card: {jp_vocab} / {en_vocab}>"
+
+    def export(self):
+        if self.jp_example and self.en_example:
+            s = self.jp_vocab + "<br>" + self.jp_example + "," + self.en_vocab + "<br>" + self.en_example + "\n"
+        else:
+            s = self.jp_vocab + "," + self.en_vocab + "\n"
+        
+        return s
 
 
 class VocabPageParser(HTMLParser):
 
 
     def handle_starttag(self, tag, attrs):
+        global CARDS
+        global CURRENT_CARD
 
-        #case when japanese vocab piece
-        if tag == "span" and attrs:
-            for key, val in attrs:
-                if key == "class" and val == "font-face-ja":
-                    self._next_is_japanese = True
 
-        #case when english translation
         if tag == "div" and attrs:
             for key, val in attrs:
-                if key == "class" and (val == "vocab-list-row__interface-language" or val == "vocab-list-row__keyphrase-interface"):
-                    self._next_is_english = True
+                if key == "class":
+                    #There are 4 unique tags used for identifying vocab pieces:
+                    #Japanese vocab:   'vocab-list-row__course-language'
+                    if val == "vocab-list-row__course-language":
+                    # All cards have JP vocab and EN vocab; when you see JP vocab,
+                    # it signals the start of a new card, so export the current one
+                    # to the "done" cards and create a new one
+                        if CURRENT_CARD:
+                            CARDS.append(CURRENT_CARD)
+                        CURRENT_CARD = Card()
+
+                        self._next_is_jp_vocab = True
+
+                    #Japanese example: 'vocab-list-row__keyphrase-course'
+                    elif val == "vocab-list-row__keyphrase-course":
+                        self._next_is_jp_example = True
+
+                    #English vocab:    'vocab-list-row__interface-language'
+                    elif val == "vocab-list-row__interface-language":
+                        self._next_is_en_vocab = True
+
+                    #English example:  'vocab-list-row__keyphrase-interface'
+                    elif val == "vocab-list-row__keyphrase-interface":
+                        self._next_is_en_example = True
+    
 
 
     def handle_data(self, data):
+        global CURRENT_CARD
         try:
-            # Case for card front
-            if self._next_is_japanese:
-                global JAPANESE_TEXT
-                JAPANESE_TEXT.append(data)
-                self._next_is_japanese = False
-            # Case for card back
-            elif self._next_is_english:
-                global ENGLISH_TEXT
-                ENGLISH_TEXT.append(data)
-                self._next_is_english = False
+            if self._next_is_jp_vocab:
+                CURRENT_CARD.jp_vocab = data
+                self._next_is_jp_vocab = False
+
+            elif self._next_is_en_vocab:
+                CURRENT_CARD.en_vocab = data
+                self._next_is_en_vocab = False
+            
+            elif self._next_is_jp_example:
+                CURRENT_CARD.jp_example = data
+                self._next_is_jp_example = False
+            
+            elif self._next_is_en_example:
+                CURRENT_CARD.en_example = data
+                self._next_is_en_example = False
         except AttributeError:
             return
 
@@ -52,21 +96,9 @@ def main():
         for line in in_f:
             p.feed(line)
 
-    global JAPANESE_TEXT
-    JAPANESE_TEXT = [x for x in JAPANESE_TEXT if not x.isdigit()]
-
     with open(OUTPUT_FILE, "w") as out_f:
-        CARD_FRONTS = JAPANESE_TEXT[::2]
-        EXAMPLE_JAPANESE = JAPANESE_TEXT[1::2]
-        CARD_BACKS = ENGLISH_TEXT[::2]
-        EXAMPLE_ENGLISH = ENGLISH_TEXT[1::2]
-        for front, example, back, translation in zip(CARD_FRONTS, EXAMPLE_JAPANESE, CARD_BACKS, EXAMPLE_ENGLISH):
-            out_f.write(front + "<br>" + example + "," + back + "<br>" + translation + "\n")
-    
-
-
-
-
+        for card in CARDS:
+            out_f.write(card.export())
 
 
 if __name__ == "__main__":
